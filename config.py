72a6d2836073c5e28892c1337c881f12180b3880
amoamirmansouri <mansouri23c@gmail.com>
2024-07-07 03:30:48 -0700

    full working project

---
+++ config.py
@@ -0,0 +1,4 @@
+API_KEY = 'freexXTdo5UaugvRyNhLj56ZTod4qexk'
+addres = 'http://api.navasan.tech/latest/?api_key='
+
+url = addres+API_KEY
--- start.py
+++ start.py
@@ -1,1 +1,20 @@
-print("hello world")
+from config import url
+
+import requests
+import json
+
+def get_rates():
+    response = requests.get(url)
+    if response.status_code == 200:
+        return json.loads(response.text)
+    return response.status_code
+
+
+def archive(filenam, rates):
+    with open(f'arc/{filenam}.json', "w") as file:
+        for i in rates:
+            file.write(json.dumps(i)+':'+str(rates[i])+'\n')
+
+if __name__ == "__main__":
+    response = get_rates()
+    archive('tody',response)
